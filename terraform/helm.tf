ab3b23a170b781bf248ae562834ec613e0806589
Dejan <dejankov@gmail.com>
2024-09-08 21:21:21 +0200

    Kubernetes to Terraform

---
+++ terraform/helm.tf
@@ -0,0 +1,32 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+# Install the NGINX Ingress Controller using Helm
+resource "helm_release" "nginx_ingress" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace  = "ingress-nginx"
+  create_namespace = true
+
+  depends_on = [module.eks]
+}
+
+# Install cert-manager using Helm
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  namespace  = "cert-manager"
+  create_namespace = true
+  version    = "v1.15.3"
+  set {
+    name  = "crds.enabled"
+    value = "true"
+  }
+
+  depends_on = [module.eks]
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,15 @@ variable "region" {
   type        = string
   default     = "eu-west-3"
 }
+
+variable "cluster_name" {
+  description = "EKS Cluster name"
+  type        = string
+  default     = "barbara"
+}
+
+variable "app" {
+  description = "Application name"
+  type        = string
+  default     = "lh-whatsapp"
+}
